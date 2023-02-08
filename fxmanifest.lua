4e65a8b9543ad287bff816385de788d827898b96
Kutay.Ozberak.Contractor <kutay.ozberak.contractor@pepsico.com>
2023-02-08 10:47:57 +0300

    change manifest file type

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,6 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+
+games { 'gta5' }
 
 description 'Yurima Dialog'
 
@@ -15,3 +17,5 @@ client_scripts {
 files {
 	'ui/*.*'
 }
+
+lua54 'yes'
